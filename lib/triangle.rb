52d5bd9967e1880e1d91bc491b263a37706bd618
Justin Winger <justinwinger4@gmail.com>
2020-04-08 19:24:05 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+ 
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def valid?
+    sum_one_two = side_1 + side_2
+    sum_one_three = side_1 + side_3
+    sum_two_three = side_2 + side_3
+
+    if !(sum_one_two > side_3 && sum_one_three > side_2 && sum_two_three > side_1) || (side_1 <= 0 && side_2 <= 0 && side_3 <= 0)
+      raise TriangleError
+    end
+  end
+
+  
+  def kind
+     valid?
+       if side_1 == side_2 && side_3 == side_2
+        return :equilateral
+      elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+        return :isosceles
+      else
+        return :scalene
+      end
+  end
+  
+  class TriangleError < StandardError
+
+  end
+  
 end
+
+
